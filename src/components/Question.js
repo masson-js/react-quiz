2b6fc16daac67202a665331857088f815ed7b7c2
Vladimir Nikulin <massonn.js@gmail.com>
2024-03-14 22:22:06 +0100

    14

---
+++ src/components/Question.js
@@ -0,0 +1,11 @@
+import Options from "./Options";
+function Question({ question ,dispatch, answer }) {
+  return (
+    <>
+      <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer}/>
+    </>
+  );
+}
+
+export default Question;
